2494be9c16b0cf3ad319f55b24c8592e176f0c20
David Acevedo <rabbit.editor@gmail.com>
2014-05-19 15:23:29 -0400

    adds some rspec specs pending specs

---
+++ spec/features/debtors_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "Entering Debtors" do
+  it "Contains a format for ss or ein"
+  it "validates phone numbers"
+end
---
+++ spec/models/debt_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Debt do
+  
+  it "Contains a permit number or infraction number"
+  it "Contains a debtor_id"
+  it "Is either paid, in process or owes some $$"
+  
+  describe "in process" do
+    it "In process it contains a payment plan (plan de pago)"
+  end
+  
+  
+end
