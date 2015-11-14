a46fde7db0ffefa726ee35cb2bdfbf8efe9f340b
David Acevedo <rabbit.editor@yahoo.com>
2015-11-14 15:35:05 -0400

    more rspec fixes

--- spec/models/fimas_account_spec.rb
+++ spec/models/fimas_account_spec.rb
@@ -2,5 +2,10 @@
 require 'spec_helper'
 
 describe FimasAccount do
-  skip "add some examples to (or delete) #{__FILE__}"
+  describe "responds to " do
+    characteristics = [:description, :code]
+    characteristics.each do |type|
+      it {should respond_to(type.to_sym)}
+    end
+  end
 end
