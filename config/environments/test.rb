79b1cf37cfb268dd26599c7ef94ce3f9245e5135
rebelwarrior <rabbit.editor@yahoo.com>
2016-04-20 20:16:59 -0400

    fixes typo on travis.yaml and adds com to config/env/test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,9 @@
 # -*- encoding : utf-8 -*-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  # For Travis CI
+  # config.active_record.maintain_test_schema = false 
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
